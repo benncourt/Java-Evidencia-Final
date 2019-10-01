b31b8fff09073e7e50b4cbfa34fbabe16bef509d
benncourt <53952589+benncourt@users.noreply.github.com>
2019-09-30 22:20:35 -0500

    Update ComponentPackageMaker.java

--- src/main/java/me/jmll/utm/component/ComponentPackageMaker.java
+++ src/main/java/me/jmll/utm/component/ComponentPackageMaker.java
@@ -1,7 +1,7 @@
 package me.jmll.utm.component;
 
 /**
- * Clase vacía para indentificar el paquete 
+ * Clase vacía para indentificar el paquete
  * que lo contiene para utilizar basePackageClasses
  *  en la anotacieón @ComponentScan
  */
